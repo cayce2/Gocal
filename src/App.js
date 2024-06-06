a52d7bab805c5962bfccf6f09f42d0e40d5cd53b
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-06-06 01:26:59 +0000

    frontend

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Layout from './components/Layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Docs from './pages/Docs'
+import History from './pages/History'
+import Settings from './pages/Settings'
+import Contacts from './pages/Contacts'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/history' element={<History />} />
+                    <Route path='/Docs' element={<Docs />} />
+                    <Route path='/Contacts' element={<Contacts />} />
+                    <Route path='/settings' element={<Settings />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    )
+}
+
+export default App;
