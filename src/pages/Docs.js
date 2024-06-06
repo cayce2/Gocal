c9846f550c3c7df4d0024b4a21322994be406d92
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-06-06 03:44:38 +0000

    docs.js

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -24,8 +24,6 @@ export default function Docs() {
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
             <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
                   <div className="flex w-0 flex-1 items-center">
                     <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                     <div className="ml-4 flex min-w-0 flex-1 gap-2">
@@ -38,8 +36,6 @@ export default function Docs() {
                       Download
                     </a>
                   </div>
-                </li>
-              </ul>
             </dd>
           </div>
         </dl>
