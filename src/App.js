4f0d095ec20b644c94a6ab613adb761b391b7103
Caleb Mumbi <36436206+cayce2@users.noreply.github.com>
2024-06-29 09:35:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+//import React, {useState} from 'react'
 import Layout from './components/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
@@ -6,10 +6,10 @@ import Docs from './pages/Docs'
 import History from './pages/History'
 import Settings from './pages/Settings'
 import Contacts from './pages/Contacts'
-import Login from './components/Login/login'
+//import Login from './components/Login/login'
 
-function App() {
-    {/* const [token, setToken] = useState();
+ {/*function App() {
+    const [token, setToken] = useState();
 
     if(!token) {
         return <Login setToken={setToken} />
